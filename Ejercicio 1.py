89a56ff5851838827529ec2202f083311ac8a002
Jose Franco <josefranber2609@gmail.com>
2026-03-23 16:49:36 -0400

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,5 @@
+nombre=input("dime tu nombre?:")
+edad=input("ahora, cual es tu edad?:")
+hobby=input("cual es tu hobby?:")
+
+print(f"te llamas {nombre}, tienes {edad} y te gusta {hobby}")
---
+++ ejercicio 2.py
@@ -0,0 +1,7 @@
+print("como calcular el area de un rectangulo")
+base= float(input("dame la base del rectangulo:"))
+altura= float(input("dame la altura del rectangulo:"))
+
+area= base * altura
+
+print(f"el area del rectangulo es {area}")
