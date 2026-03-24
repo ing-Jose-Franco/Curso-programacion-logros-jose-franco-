2938460f79d10644fbe334446b00eae28ba6bb75
ing-jose-franco <josefranber2609@gmail.com>
2026-03-24 16:40:06 -0400

    cambios

--- ejercicios modulo 1/ejercicio verdadero o falso.py
+++ ejercicios modulo 1/ejercicio verdadero o falso.py
@@ -1,2 +1,4 @@
 print("verdadero o falso")
-persona= input("estudiante o trabajador?:")
+es_estudiante= True
+tiene_Trabajo= False
+print(f"Hola soy jose, soy estudiante {es_estudiante} y no tengo chamba {tiene_Trabajo}")
