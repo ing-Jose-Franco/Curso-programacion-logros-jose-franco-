bcb90b8c73ed8cdaa2170691e56c02bf5ece5d3b
ing-jose-franco <josefranber2609@gmail.com>
2026-03-24 14:29:40 -0400

    creacion de carpeta

--- Ejercicio 1.py
+++
@@ -1,5 +0,0 @@
-nombre=input("dime tu nombre?:")
-edad=input("ahora, cual es tu edad?:")
-hobby=input("cual es tu hobby?:")
-
-print(f"te llamas {nombre}, tienes {edad} y te gusta {hobby}")
--- ejercicio 2.py
+++
@@ -1,8 +0,0 @@
-print("como calcular el area de un rectangulo")
-base= float(input("dame la base del rectangulo:"))
-altura= float(input("dame la altura del rectangulo:"))
-
-area= base * altura
-
-print(f"el area del rectangulo es {area}")
-print("problema resuelto!")
