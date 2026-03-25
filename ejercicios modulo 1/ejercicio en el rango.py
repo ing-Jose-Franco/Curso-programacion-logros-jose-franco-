8eb3e5f3f1522cce302093bd7e191c65b6706a1a
ing-jose-franco <josefranber2609@gmail.com>
2026-03-25 01:18:04 -0400

    cambios

---
+++ ejercicios modulo 1/ejercicio en el rango.py
@@ -0,0 +1,7 @@
+print("En el rango")
+
+num= float(input("elige un numero: "))
+
+resultado= (num > 10 and num < 20)
+
+print(f"tu numero es {num} y es {resultado}")
---
+++ ejercicios modulo 1/ejercicio maquina del tiempo.py
@@ -0,0 +1,7 @@
+print("Maquina del tiempo")
+
+dias= int(input("cuantos dias deseas contar?: "))
+
+resultado= dias * 24 * 60 * 60
+
+print(f"{dias} dias son en total {resultado} segundos")
