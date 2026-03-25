f1f5310327d67e6120bbb107a78dc39f567e8b12
ing-jose-franco <josefranber2609@gmail.com>
2026-03-25 01:06:41 -0400

    cambios

---
+++ ejercicios modulo 1/ejercicio rendimiento de motor.py
@@ -0,0 +1,8 @@
+print("Rendimiento de motor")
+
+kilometros=float (input("cuantos kilometros deseas viajar?: "))
+litros=float(input("cuantos litros consume tu motor cada kilometro?: "))
+
+calculo= kilometros * litros
+
+print(f"Si deseas viajar {kilometros} kilometros necesitaras {calculo} litros de gasolina")
