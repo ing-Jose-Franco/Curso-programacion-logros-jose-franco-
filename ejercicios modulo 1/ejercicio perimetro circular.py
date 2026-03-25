f698095d3eac86230c842b818b9618373be73c44
ing-jose-franco <josefranber2609@gmail.com>
2026-03-25 00:34:57 -0400

    cambios

---
+++ ejercicios modulo 1/ejercicio perimetro circular.py
@@ -0,0 +1,8 @@
+print("Perimetro circular")
+
+pi= 3.1416
+radio= float(input("cual es el radio de la circunferencia?: "))
+
+perimetro= 2 * pi * radio
+
+print(f"El perimetro de la circunferencia es {perimetro}")
