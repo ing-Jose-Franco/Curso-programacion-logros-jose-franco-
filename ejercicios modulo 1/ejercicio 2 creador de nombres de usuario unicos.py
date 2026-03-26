87f523a0aa365f445a08eed498759e8d48810ec8
ing-jose-franco <josefranber2609@gmail.com>
2026-03-26 19:58:34 -0400

    cambios

---
+++ ejercicios modulo 1/ejercicio 2 creador de nombres de usuario unicos.py
@@ -0,0 +1,17 @@
+nombres_prohibidos= ("admin", "root", "boss", "staff", "security")
+
+nombres_rechazados= []
+
+usuario= input("Introduce un nombre de usuario: ").lower()
+
+while usuario in nombres_prohibidos:
+    print(f"Lo siento, '{usuario}' es un nombre reservado.")
+    
+    nombres_rechazados.append(usuario)
+    
+    usuario= input("Prueba con otro nombre: ").lower()
+
+print(f"\nFelicidades!, Tu nuevo usuario es: {usuario}")
+
+if nombres_rechazados:
+    print(f"Historial de intentos rechazados: {nombres_rechazados}")
