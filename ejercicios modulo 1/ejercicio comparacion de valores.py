bff844af6fb201da96d3f8153cf99fd2044159bd
ing-jose-franco <josefranber2609@gmail.com>
2026-03-25 20:27:03 -0400

    cambios

---
+++ ejercicios modulo 1/ejercicio comparacion de valores.py
@@ -0,0 +1,13 @@
+print("Comparacion de valores")
+
+num1=int(input("Elige el primer numero: "))
+num2=int(input("Elige el segundo numero: "))
+
+if num1 > num2:
+    print(f"El numero {num1} es mayor que {num2}")
+
+elif num1 == num2:
+    print(f"Los dos numeros son iguales")
+
+else:
+    print(f"El numero {num2} es mayor que {num1}")
---
+++ ejercicios modulo 1/ejercicio verificacion de paridad.py
@@ -0,0 +1,8 @@
+print("Verificacion de paridad")
+
+numero=int(input("Elige un numero: "))
+
+if numero % 2 == 0:
+    print("Es par")
+else:
+    print("Es impar")
