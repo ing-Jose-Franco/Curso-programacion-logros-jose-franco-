46e5e7d978d11a04b7ee15ad021c413547e05296
ing-jose-franco <josefranber2609@gmail.com>
2026-03-26 15:32:06 -0400

    cambios

---
+++ ejercicios modulo 1/ejercicio cuento interactivo.py
@@ -0,0 +1,7 @@
+print("Cuento interactivo")
+
+print("\n**EL CABALLERO NO-MUERTO**")
+
+print("\nEn esta historia encarnamos a aquel no-muerto elegido por llama para volver a la vida,\nen busca de desatar la destruccion y la oscuridad por toda las tierras de lordran o\nde liberarla de yugo del la llama.")
+
+print("Despiertas en una celda tenue y mal oliente, notas que las rejas estan desgastadas por los\naños, parecen que pudiera caer la puerta con un golpe con la suficiente fuerza, ")
