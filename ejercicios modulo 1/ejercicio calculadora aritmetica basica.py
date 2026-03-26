83ac276cdf4d1bd5a933b2806d463df6843d87c8
ing-jose-franco <josefranber2609@gmail.com>
2026-03-25 21:28:58 -0400

    cambios

--- ejercicios modulo 1/ejercicio calculadora aritmetica basica.py
+++ ejercicios modulo 1/ejercicio calculadora aritmetica basica.py
@@ -6,20 +6,22 @@ num2=float(input("Elige el segundo numero para la operacion: "))
 operacion=(input("\n¿QUE OPERACION DESEAS REALIZAR?\n\n SUMA\n RESTA\n MULTIPLICACION\n DIVISION\n\n")).lower
 
 if operacion == "suma":
-    suma= round((num1 + num2), 2)
-    print(f"La Suma de los 2 numeros es {suma}")
+    sum= round((num1 + num2), 2)
+    print(f"La Suma de los 2 numeros es {sum}")
 
 elif operacion == "resta":
-    resta= round((num1 - num2), 2)
-    print(f"La Resta de los 2 numeros es {resta}")
+    rest= round((num1 - num2), 2)
+    print(f"La Resta de los 2 numeros es {rest}")
 
 elif operacion == "multiplicacion":
-    multiplicacion= round((num1 * num2), 2)
-    print(f"La multiplicacion de los 2 numeros es {multiplicacion}")
+    multi= round((num1 * num2), 2)
+    print(f"La multiplicacion de los 2 numeros es {multi}")
 
 elif operacion == "division":
     if num1 != 0 and num2 !=0:
-        division= round((num1 / num2), 2)
-        print(f"La division de los 2 numeros es {division}")
+        div= round((num1 / num2), 2)
+        print(f"La division de los 2 numeros es {div}")
     else:
         print("No se puede dividir por 0")
+else:
+    print("ERROR")
