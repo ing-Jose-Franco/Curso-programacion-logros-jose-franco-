5e441d3c67df27cb9c0004438722797e96dce37c
ing-jose-franco <josefranber2609@gmail.com>
2026-03-26 20:26:25 -0400

    cambios

---
+++ ejercicios modulo 1/ejercicio 5 verificador de contraseñas seguras.py
@@ -0,0 +1,28 @@
+rango_longitud = (8, 20)
+caracteres_especiales = ("!", "@", "#", "$", "%", "*", ".")
+
+contraseñas= input("Introduce contraseñas separadas por comas (ej: 123!, admin., CarlitosElPro777#): ")
+lista_passwords = contraseñas.split(",")
+
+contraseñas_seguras = []
+
+for contra in lista_passwords:
+    contra = contra.strip()
+    largo = len(contra)
+    
+    tiene_especial = False
+    for especial in caracteres_especiales:
+        if especial in contra:
+            tiene_especial = True
+            break
+            
+    if (largo >= rango_longitud[0] and largo <= rango_longitud[1]) and tiene_especial:
+        contraseñas_seguras.append(contra)
+    else:
+        print(f"La contraseña '{contra}' ha sido rechazada.")
+
+print("\n--- Registro de Contraseñas Seguras ---")
+if  contraseñas_seguras:
+    print(f"Válidas: {contraseñas_seguras}")
+else:
+    print("Ninguna contraseña cumplió con los estándares de seguridad.")
