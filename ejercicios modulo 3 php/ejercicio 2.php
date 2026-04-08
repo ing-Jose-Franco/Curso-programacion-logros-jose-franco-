e38e31b11fc15cc00b7806f664a4e24ff7fc9f31
ing-jose-franco <josefranber2609@gmail.com>
2026-04-07 20:06:28 -0400

    cambios

--- ejercicios modulo 3 php/ejercicio 2.php
+++ ejercicios modulo 3 php/ejercicio 2.php
@@ -1,11 +1,10 @@
 <?php
 $tabla = 8;
 
-echo "TABLA DE MULTIPLICAR DEL $tabla"
+echo "TABLA DE MULTIPLICAR DEL $tabla\n";
 
-for ($i = 1; $i <= 10; $i++) {
+for ($i = 1; $i <= 10; $i++) { 
     $resultado = $tabla * $i;
-    // Usamos el formato "8 x i = resultado"
     echo "$tabla x $i = $resultado\n";
 }
 ?>
